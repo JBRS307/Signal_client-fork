fc0173148abf14a748411148e08dec2b4e44a736
hytrek <hytrek@student.agh.edu.pl>
2024-06-10 18:15:20 +0200

    Wysyłanie to nowa funkcja

--- signal_client/src/main.rs
+++ signal_client/src/main.rs
@@ -14,7 +14,7 @@ use futures::channel::oneshot;
 use futures::future;
 use presage::libsignal_service::configuration::SignalServers;
 use presage::libsignal_service::content::ContentBody;
-
+use tokio::runtime::Runtime;
 
 fn find_account_uuid(phone_number: &str) -> Option<Uuid> {
     let mut file = File::open("/Users/michalinahytrek/Documents/Signal_client/signal_client/registration/accounts.json").expect("Unable to open file");
@@ -32,57 +32,30 @@ fn find_account_uuid(phone_number: &str) -> Option<Uuid> {
     None
 }
 
-
-// odkomentuj jak zarejestrujesz linkiem z drugiego main
-
-
-#[tokio::main(flavor = "multi_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Pobierz argumenty z linii komend
-    let args: Vec<String> = env::args().collect();
-
-    let sender = &args[1];
-    let recipient = &args[2];
-    let message = &args[3];
-
-    // let sender = "+48574596401";
-    // let recipient = "+48574596401";
-    // let message = "Nowa wiadomosc";
-
+async fn send_message(arguments: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
+    let recipient = &arguments[2];
+    let message = &arguments[3];
 
     // Inicjalizacja sklepu Sled
     let store = SledStore::open("/tmp/presage-example/", MigrationConflictStrategy::BackupAndDrop, OnNewIdentity::Trust)?;
-
-
     // Załaduj zarejestrowane konto nadawcy
     let mut manager = Manager::load_registered(store.clone()).await?;
+    println!("Haha");
 
-    let mut registration_data = manager.registration_data();
-    println!("{:?}", registration_data.device_name);
-    println!("{:?}", registration_data.phone_number);
-    println!("{:?}", registration_data.service_ids);
-    println!("{:?}", registration_data.device_id);
-
-    // Pobierz czas obecny jako timestamp
     let start = SystemTime::now();
     let since_the_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
     let timestamp = since_the_epoch.as_secs();
 
-    println!("{}", timestamp);
-
-
-    // Utwórz ServiceAddress z PhoneNumber
+    println!("Bebe");
 
     if let Some(uuid) = find_account_uuid(recipient) {
         let service_address = presage::libsignal_service::ServiceAddress::from(uuid);
-        let service_address = presage::libsignal_service::ServiceAddress::from(uuid);
 
         let data_message = presage::proto::DataMessage {
-            body: Some(message.parse().unwrap()), timestamp: Option::from(timestamp),
+            body: Some(message.parse().unwrap()), timestamp: Some(timestamp),
             ..Default::default()
         };
-
-
+        println!(":(");
         // Wyślij wiadomość
         manager.send_message(
             service_address,
@@ -90,10 +63,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             timestamp
         ).await?;
 
-        println!("Wiadomość wysłana od {} do {}: \"{}\"", sender, recipient, message);
+        println!("Wiadomość wysłana do {}: \"{}\"", recipient, message);
     } else {
         eprintln!("Nie znaleziono konta dla numeru odbiorcy.");
     }
+
+    Ok(())
+}
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    let option = &args[1];
+    match option.as_str() {
+        "send" => send_message(args).await?,
+        _ => {}
+    };
 
     Ok(())
 }
